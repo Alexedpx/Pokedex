1b9a3df2f96e4e0316aa78a5e965dc48c854cdc9
Alexedpx <alexandradepourtoux93@gmail.com>
2023-10-09 17:22:11 +0200

    exercice pokedex terminé

---
+++ my-react-app/src/components/PokemonCard.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+    
+return ( 
+    <figure>
+        <h1>PokemonCard</h1>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+
+    <figcaption>Bulbasaur</figcaption>
+      
+      </figure>
+);
+}
+  
+  export default App
+
+
