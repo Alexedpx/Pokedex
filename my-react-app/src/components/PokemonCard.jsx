3c8bc868b9f076352767648c357299421b478e2e
Alexedpx <alexandradepourtoux93@gmail.com>
2023-10-10 10:23:12 +0200

    pokedex modifié, avec ajout de mew

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,17 +1,38 @@
 
-function App() {
-    
-return ( 
-    <figure>
-        <h1>PokemonCard</h1>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
-    <figcaption>Bulbasaur</figcaption>
+function PokemonCard() {
+  
+
+  const pokemon = pokemonList[1];
+
+    return ( 
+      <>
+    <figure>
+      
+      {pokemon.imgSrc === undefined ? <p>???</p> :
+      
+      <img src= {pokemon.imgSrc} alt="pokemon" />}
       
       </figure>
-);
+     
+     <figcaption>{pokemon.name}</figcaption>
+      
+     </>
+      
+    );
+
 }
   
-  export default App
+  export default PokemonCard
 
 
